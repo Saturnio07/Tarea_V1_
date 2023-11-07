d68de1e3b5327c450a8de319f460d18d9176c37e
Dennis Sánchez Morales <97916956+Saturnio07@users.noreply.github.com>
2023-11-06 18:13:26 -0600

    Cambios proyecto
    Se sube el proyecto de la profe

--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -2,32 +2,30 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
-import java.util.List;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name="producto")
-public class Producto implements Serializable
-{
+public class Producto implements Serializable {
     private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_producto")
-    private long idProducto;
-    @Column(name="id_categoria")
-    private long idCategoria;
+    private Long idProducto;
+    //private Long idCategoria;  ya no se usa por el @manyToOne
     private String descripcion;
     private String detalle;
     private double precio;
     private int existencias;
-    @Column(name="ruta_imagen")
     private String rutaImagen;
-    private boolean activo; 
+    private boolean activo;
 
-    @OneToMany
-    @JoinColumn(name="id_categoria",updatable=false)
-    List<Producto> productos;
+    @ManyToOne
+    @JoinColumn(name="id_categoria")
+    Categoria categoria;
+
 
     public Producto() {
     }
@@ -36,4 +34,5 @@ public class Producto implements Serializable
         this.descripcion = descripcion;
         this.activo = activo;
     }
+    
 }
